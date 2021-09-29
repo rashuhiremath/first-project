133c9a542859e6b1263e376918f7b01f0d175bd6
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-09-29 12:36:56 +0200

    updated

--- src/Components/LatestRelease.jsx
+++ src/Components/LatestRelease.jsx
@@ -1,36 +1,27 @@
-import { Component } from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import books from '../Data/Books.json'
-import { Card } from "react-bootstrap"
-
+import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import books from "../Data/Books.json";
+import { Card } from "react-bootstrap";
 
 class LatestRelease extends Component {
-    render(){
-
-        return(
-            <Container>
-                <Row>
-                  <Col md={4} lg={3}> 
-                  {
-                      books.map(book => (
-                        <Card style={{ width: '18rem' }} key={book.id}>
-                        <Card.Img variant="top" src={book.img} />
-                        <Card.Body>
-                          <Card.Title>{book.title}</Card.Title>
-                          <Card.Text>
-                          {book.category}
-                          </Card.Text>
-                          
-                        </Card.Body>
-                      </Card>
-
-                      ))
-        }
-      
-      </Col> 
-      </Row>
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md={4} lg={3}>
+            {books.map((book) => (
+              <Card style={{ width: "14rem" }} key={book.id}>
+                <Card.Img variant="top" src={book.img} />
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text>{book.category}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+        </Row>
       </Container>
-      
-    )}
+    );
+  }
 }
-export default LatestRelease
+export default LatestRelease;
