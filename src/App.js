9f142e0c5ebf4fc5c269291e80b8dac9e8ec2470
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-10-08 10:41:05 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -6,32 +6,56 @@ import LatestRelease from "./Components/LatestRelease";
 import MyFooter from "./Components/MyFooter";
 import FetchedComment from "./Components/FetchedComment";
 import SingleBook from "./Components/SingleBook";
-import Books from "./Data/Books.json"
+import Books from "./Data/Books.json";
 import CommentArea from "./Components/CommentArea";
 import { Col, Row } from "react-bootstrap";
-import { BrowserRouter,Route } from "react-router-dom"
-import Registration from "./Components/Registration"
+import { BrowserRouter, Route } from "react-router-dom";
+import Registration from "./Components/Registration";
 import { useState } from "react";
-function App() {
 
-  const [form, setForm] = useState({name: "", surname:"", email:"",password:"",confirmPassword:""})
- 
+const BookDetailsPage = () => {
+  return (
+    <>
+      {" "}
+      <SingleBook book={Books[0]} />
+      <CommentArea />
+      <FetchedComment />
+      <LatestRelease />{" "}
+    </>
+  );
+};
+
+function App() {
+  const [form, setForm] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
   return (
     <div>
       <BrowserRouter>
-      <MyNav />
-      <Welcome />
-      <Row className="justify-content-center col col-md-4">
-        <Col >
-        <Route path="/registration" exact  render={(routerProps) => <Registration {...routerProps} form={form} />}/>
-        <SingleBook book={Books[0]}/>
-         <CommentArea /> 
-      <FetchedComment /> 
-      </Col>
-      </Row>
-      <LatestRelease  />  
-      <MyFooter />
-    </BrowserRouter>
+        <MyNav />
+        <Route path="/home" component={Welcome} />
+
+        {/* the component registration will ONLY render when your URL is /registration */}
+        <Route
+          path="/registration"
+          exact
+          render={(routerProps) => (
+            <Registration {...routerProps} form={form} />
+          )}
+        />
+        {/* Everything that is outside of the route will ALWAYS RENDER */}
+        {/* So the goal is to create another route like /home that will render the components that Only must be rendered 
+        in the home
+        */}
+        <Route path="/books/:bookId" component={BookDetailsPage} /> 
+
+        <MyFooter />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -3,24 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { withRouter } from "react-router";
-const Registration = ({Location ,form,setForm}) => {
-    {/*  const [nameChar,setNameChar]=useState('')
-    const [surnameChar,setSurnameChar]=useState('')
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    const [confirmPassword,setConfirmPassword]=useState('') 
-*/}
- 
-   
+const Registration = ({ Location, form, setForm }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = (e)=>{
-    e.preventDefault()
-
-
-        
-    }
-
-   { /* useEffect(()=>{
+  {
+    /* useEffect(()=>{
         const isFormComplete = () => {
             if(
                nameChar.length > 2 && 
@@ -30,43 +19,85 @@ const Registration = ({Location ,form,setForm}) => {
             )
            }  
 
-    },[]) */}
-     
+    },[]) */
+  }
+
+  const handleChange = (e) => {
+    // instead of handle change it should be send form and then do the post request
+    setForm(...form 
+        )
+    
+}
 
   return (
     <>
       <Container>
         <Row>
           <Col>
-            <Form onSubmit>
-            <Form.Group controlId="formBasicName">
+            <Form onSubmit={handleChange}>
+              <Form.Group controlId="formBasicName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="name"  value={form.name} onChange={e => setForm(e.target.value)} placeholder="Enter name" required />
-                
+                <Form.Control
+                  type="name"
+                  value={form.name}
+                //   the setForm is overwroiting your form you are not keeping previous information
+                  onChange={(e) => setForm({
+                      ...form,                
+                         name:e.target.value}
+                )}
+                  placeholder="Enter name"
+                  required
+                />
               </Form.Group>
               <Form.Group controlId="formBasicSurname">
                 <Form.Label>Surname</Form.Label>
-                <Form.Control type="surname"  value={form.surname} onChange={e => setForm(e.target.value)} placeholder="Enter surname" required />
-                
+                <Form.Control
+                  type="surname"
+                  value={form.surname}
+                  onChange={(e) =>  setForm({
+                    ...form,                
+                       surname:e.target.value})}
+                  placeholder="Enter surname"
+                  required
+                />
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" value={form.email} onChange={e => setForm(e.target.value)} placeholder="Enter email" required />
-                
+                <Form.Control
+                  type="email"
+                  value={form.email}
+                  onChange={(e) =>  setForm({
+                    ...form,                
+                       email:e.target.value})}
+                  placeholder="Enter email"
+                  required
+                />
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password"  value={form.password} onChange={e => setForm(e.target.value)} placeholder="Password" required />
+                <Form.Control
+                  type="password"
+                  value={form.password}
+                  onChange={(e) => setForm(e.target.value)}
+                  placeholder="Password"
+                  required
+                />
               </Form.Group>
               <Form.Group controlId="formBasicConfirmPassword">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="confirmPassword" value={form.confirmPassword} onChange={e => setForm(e.target.value)} placeholder="confirm Password" required/>
+                <Form.Control
+                  type="confirmPassword"
+                  value={form.confirmPassword}
+                  onChange={(e) => setForm(e.target.value)}
+                  placeholder="confirm Password"
+                  required
+                />
               </Form.Group>
-              
+
               {/* disabled={!isFormComplete()} */}
 
-              <Button variant="primary" type="submit" >
+              <Button variant="primary" type="submit">
                 Submit
               </Button>
             </Form>
